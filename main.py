bc25ffadbed6730ce4a0b2d7b198e8398c2860da
arashHarirpoosh <aharirpoosh@gmail.com>
2020-04-02 17:40:34 +0430

    Saving The Results.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Lexer import Lexer
 lexer = Lexer().build()
 file = open('TestCases/test1.txt')
+print(file.name)
+res = open('Results/result'+file.name.split('/')[-1].title(), 'w')
 text_input = file.read()
 file.close()
 lexer.input(text_input)
@@ -8,3 +10,5 @@ while True:
     tok = lexer.token()
     if not tok: break
     print(tok)
+    res.write(str(tok)+'\n')
+
